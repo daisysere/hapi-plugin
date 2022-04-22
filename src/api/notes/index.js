90254ef18544c512e8e51c536b06b499afe455d9
Daisy Sere <daisyseredams@gmail.com>
2022-04-22 19:09:50 +0700

    2. validator

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -8,8 +8,8 @@ const routes = require('./routes');
 module.exports = {
   name: 'notes',
   version: '1.0.0',
-  register: async (server, { service,validator }) => {
-    const notesHandler = new NotesHandler(service,validator);
+  register: async (server, { service, validator }) => {
+    const notesHandler = new NotesHandler(service, validator);
     server.route(routes(notesHandler));
   },
 };
--- src/validator/notes/schema.js
+++ src/validator/notes/schema.js
@@ -1,1 +1,9 @@
-// File ini berfokus untuk membuat dan menulis objek schema data notes
+const Joi = require('joi');
+
+const NotePayloadSchema = Joi.object({
+  title: Joi.string().required(),
+  body: Joi.string().required(),
+  tags: Joi.array().items(Joi.string()).required(),
+});
+
+module.exports = NotePayloadSchema;
